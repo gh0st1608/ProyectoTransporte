3cf3b2803be116bbd5902e83e3cb7ca1eac08e50
Ubuntu <ubuntu@ip-172-31-13-101.ec2.internal>
2022-04-27 05:43:13 +0000

    1c

---
+++ conf/www/Expreso/config/db.php
@@ -0,0 +1,10 @@
+﻿<?php
+ define('DB_HOST', 'localhost');//DB_HOST:  generalmente suele ser "127.0.0.1"
+ define('DB_USER', 'root');//Usuario de tu base de datos
+ define('DB_PASS', 'passw0rd');//Contraseña del usuario de la base de datos
+ define('DB_NAME', 'transporte');//Nombre de la base de datosdb_sis_integral
+ //define('DB_NAME', 'db_sis_integral');//Nombre de la base de datos
+
+
+?>
+
